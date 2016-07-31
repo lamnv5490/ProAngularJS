9d73a3badec15524489be43a2e1185872b381a5a
LamNguyen <lamnv5490@gmail.com>
2016-07-31 12:50:02 +0700

    ss

---
+++ NgBook/Example/Example/Example/IsolatedScope/index.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="../../Scripts/angular.min.js" />
+
+var app = angular.module("phoneApp", []);
+
+app.controller("AppController", function ($scope) {
+    $scope.callHome = function (message) {
+        alert(message);
+    };
+
+    $scope.annouceCall = function (message) {
+        alert(message);
+    };
+});
+
+app.directive("phone", function () {
+    return {
+        scope: {
+            dial: "&"
+        },
+        template: '<input type="text" ng-model="value">' +
+                  '<div class="button" ng-click="dial({hello:value})">' +
+                  'Call home!</div>',
+    }
+});
